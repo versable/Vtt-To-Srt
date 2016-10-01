02e70f277cca7b2f29d713a88ad794466340d29a
Michael Langfermann <versable@gmail.com>
2016-10-01 14:23:59 +0200

    Add perl script for converting vtt files to srt files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Vtt-To-Srt
-VTT to SRT subtitle converter script
+# Vtt To Srt
+
+*Vtt To Srt* is an open source perl script, used to convert *WEBTT* subtitle files to *SRT* files. It does not use any external perl modules and is extremely fast.
+
+## Usage
+The script outputs directly to `STDOUT`:
+
+    ./vtttosrt.pl example.vtt > example.srt
---
+++ vtttosrt.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+my $input = $ARGV[0];
+
+# Check the input
+die "Usage sample: $0 example.vtt > example.srt\n" unless $input;
+die "File \`$input\` does not exist\n" unless -e $input;
+
+# Open the input file
+open( my $fh, "<", $input )
+    or die "Can't open < $input: $!\n";
+
+my $output = '';
+for ( my $item = 0, my $line; $line = <$fh>; )
+{
+    # Ignore first line if it has WEBVTT declaration
+    next if $item == 0 && $line =~ /^WEBVTT$/;
+
+    # Expand time string if it is too short
+    $line =~ s/(^|\s)(\d+)\:(\d+)\./${1}00:$2:$3\./g;
+
+    # Print and iterate item number if regex time string replacement succeeds
+    print ++$item . "\n"
+        if $line =~ s/(\d+)\:(\d+)\:(\d+)\.(\d+)/$1:$2:$3,$4/g;
+    print $line;
+}
